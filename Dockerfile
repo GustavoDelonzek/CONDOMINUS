958e47c6b361a7a6ffde7b7e6fb8ac28c893d112
GustavoDelonzek <gustavo2016delonzek@gmail.com>
2025-12-01 21:54:56 -0300

    setup: config auth and api for project

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM php:8.4-fpm
 
+ARG UID=1000
+ARG GID=1000
+
 RUN apt-get update && apt-get install -y \
     libpng-dev \
     libonig-dev \
@@ -13,6 +16,9 @@ RUN apt-get update && apt-get install -y \
 RUN docker-php-ext-install pdo_pgsql mbstring exif pcntl bcmath gd sockets zip \
     && pecl install redis \
     && docker-php-ext-enable redis
+
+RUN groupmod -g $GID www-data && \
+    usermod -u $UID -g $GID www-data
 
 COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
 
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,8 +6,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -44,5 +45,21 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     * Pega o identificador que será guardado no token (geralmente o ID do user)
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Adiciona dados extras no token se precisar (aqui deixamos vazio)
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
